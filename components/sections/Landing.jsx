ca5220feb50e9612b911d4d6959235953d113a08
ansagang <ansagaang@gmail.com>
2023-01-26 01:23:40 +0600

    components & pages

---
+++ components/sections/Landing.jsx
@@ -0,0 +1,13 @@
+export default function Landing() {
+    return (
+        <section className="landing">
+            <div className="container">
+                <div className="landing__inner">
+                    <div className="landing__title title">
+                        <h1>Landing</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
