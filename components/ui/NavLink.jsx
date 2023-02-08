6e3bdef7d8ba8ecb18dee5e706532f21cd495bae
ansagang <ansagaang@gmail.com>
2023-02-09 00:34:44 +0600

    ui changes

--- components/ui/NavLink.jsx
+++ components/ui/NavLink.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function NavLink({ href, children }) {
     const router = useRouter();
-    const className = router.asPath === `${href}` ? "active" : '';
+    const className = router.asPath.split('?')[0].split('/')[1] === `${href.split('?')[0].split('/')[1]}` ? "active" : '';
     return (
         <Link href={href} className={className}>
             {children}
